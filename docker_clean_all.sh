f1f328c06b90152aaf6f64796a6fc39caacbcaa2
Y.S.Shih <yusen.shih@gmail.com>
2021-05-06 18:49:43 +0800

    edit clean shell

--- docker_clean_all.sh
+++ docker_clean_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# 2019.1.16 Wed Anderson Version 0.0.1
+# remove all containers+docker images
+#./docker_stop_container.sh
+#./docker_rm_container.sh
+#./docker_rm_images.sh
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi -f $(docker images -aq)
+docker ps -a
+docker images
